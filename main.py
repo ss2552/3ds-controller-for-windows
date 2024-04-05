a952a08867dd30287435046f0c39714aa7d89702
ss2552 <85072753+ss2552@users.noreply.github.com>
2024-04-05 21:26:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,18 @@ debug = True
 
 send = lambda set: requests.post("https://script.google.com/macros/s/AKfycbwvhNBhf5_kf7Yw2e6-evI6WjyjoadpVFgm8WQ6W1YGmr5wEhmWILGq-gnyuZxfTIyu/exec", headers={'Content-Type': 'charset=UTF-8'},data=str(set))
 try:
+    c = send("u")
     if send("ping") == 1:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.bind(("", 8889))
         while True:
             data, addr = s.recvfrom(4096)
-            match bytearray(data)[0]:
+            rawdata = bytearray(data)[0]
+            match rawdata:
                 case 0:
                     pass
                 case 1:
-                    key = struct.unpack(send("u"), rawdata) # "<BBxxIhhHHhh"
+                    key = struct.unpack(c, rawdata) # "<BBxxIhhHHhh"
                     if  key[3]:
                         case 28:
                             pag.press('d')
@@ -29,9 +31,9 @@ try:
                             pag.press('w')
                         case 31:
                             pag.press('s')
-                    if debug:
-                        
-                        print(key)
+            if debug:
+                send(key)
+                print(key)
     exit()
 except Exception as e:
     print(e)
